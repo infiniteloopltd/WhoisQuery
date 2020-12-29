5b256ec5b2b252673cb1401af59853e5f6af2fde
Fiach Reid <fiach@globaldataconsortium.com>
2020-12-29 12:34:57 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using Whois.NET;
+
+namespace Whois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://www.lacnic.net/2472/2/lacnic/request-bulk-whois-access
+            // https://afrinic.net/support/general-queries/how-can-i-request-for-bulk-whois-data
+            // https://www.ripe.net/manage-ips-and-asns/db/nrtm-mirroring
+            // https://www.arin.net/reference/research/bulkwhois/
+            // https://www.apnic.net/manage-ip/using-whois/bulk-access/
+
+
+            var domain = "targa.co.it";
+            var response = WhoisClient.Query(domain, encoding: Encoding.UTF8);
+            Console.WriteLine(response);
+        }
+    }
+}
